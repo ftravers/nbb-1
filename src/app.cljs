87fb92b9d45fa6e1b3ee6cfa21bcdaffdf594504
Fenton Travers <fenton.travers@gmail.com>
2023-01-26 11:21:34 -0700

    .

--- src/app.cljs
+++ src/app.cljs
@@ -1,8 +1,5 @@
 (ns app
-  (:require ["moment" :as moment]))
-
-;; moment/default
-;; clojure.string/tr
+  (:require ["moment$default" :as moment]))
 
 (+ 1 1)
 
@@ -10,3 +7,8 @@
 
 (abcd 1)
 
+(prn (js->clj (moment/max)))
+( (.max (moment)))
+;; => #object[Moment Wed Jan 25 2023 18:57:16 GMT-0700]
+(let [a (moment/max)]
+  (.toString a)) 
